3fd3bb34d0497ee2eb395651bc457f9b7af5de4c
Tim G. Thomas <tim@timgthomas.com>
2022-08-31 20:02:24 -0500

    migrate existing site to svelte

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,30 @@
+<svelte:head>
+  <link rel="stylesheet" href="/global-sub.css">
+  <title>Faire Day &middot; Faire and Festival Weather</title>
+</svelte:head>
+
+<h1>Faire Day</h1>
+<main>
+  <p><a href="/">&lsaquo; Home</a></p>
+  <p>Hi! I’m Tim, the creator of Faire Day.</p>
+  <h2>Privacy Policy</h2>
+  <p>Your privacy is of utmost importance to me, so I want to be clear that:</p>
+  <ul>
+    <li>Faire Day does not directly collect any data about you or your use of the app. That includes this page!</li>
+    <li>If enabled, Apple shares some analytics (such as crash data) with me. I do not share this data. You can opt-out in <strong>Settings > Privacy > Analytics & Improvements</strong>.</li>
+  </ul>
+  <p>If you have any questions about privacy, <a href="mailto:faireday@timgthomas.com">shoot me an email</a>!</p>
+  <p>Last updated 25 Dec 2019.</p>
+</main>
+<a class="app-store" href="https://apple.co/3fDEqho">Download on the App Store</a>
+<div class="social">
+  <a class="facebook" href="https://fb.me/faireday" target="_blank" rel="noopener noreferrer">
+    Like Faire Day on Facebook
+  </a>
+  <a class="twitter" href="https://twitter.com/faireday" target="_blank" rel="noopener noreferrer">
+    Follow Faire Day on Twitter
+  </a>
+  <a class="discord" href="https://discord.gg/zNWpHHGnhw" target="_blank" rel="noopener noreferrer">
+    Join the Faire Day server on Discord
+  </a>
+</div>
--- src/routes/support/+page.svelte
+++ src/routes/support/+page.svelte
@@ -1,18 +1,17 @@
 <svelte:head>
-  <link rel="stylesheet" href="/global-home.css">
+  <link rel="stylesheet" href="/global-sub.css">
   <title>Faire Day &middot; Faire and Festival Weather</title>
 </svelte:head>
 
 <h1>Faire Day</h1>
+<main>
+  <p><a href="/">&lsaquo; Home</a></p>
+  <p>Hi! I’m Tim, the creator of Faire Day.</p>
+  <h2>Need Help?</h2>
+  <p>If you're having any issues with the app, don't hesitate to <a href="mailto:faireday@timgthomas.com">email me</a> or visit Faire Day's Facebook page and <a href="https://fb.me/faireday">send me a message</a>.</p>
+</main>
 <a class="app-store" href="https://apple.co/3fDEqho">Download on the App Store</a>
-<nav>
-  <a href="/support">Support</a>
-  <a href="/privacy">Privacy</a>
-</nav>
 <div class="social">
-  <a class="patreon" href="https://www.patreon.com/faireday" target="_blank" rel="noopener noreferrer">
-    Support Faire Day on Patreon
-  </a>
   <a class="facebook" href="https://fb.me/faireday" target="_blank" rel="noopener noreferrer">
     Like Faire Day on Facebook
   </a>
