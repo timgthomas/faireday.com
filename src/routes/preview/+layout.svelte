3a8a090b7efa150d7d83ee3e1ceb923a0b2e0f70
Tim G. Thomas <tim@timgthomas.com>
2022-09-04 13:00:05 -0500

    switch to local fonts

--- src/routes/preview/+layout.svelte
+++ src/routes/preview/+layout.svelte
@@ -1,4 +1,9 @@
 <script>
+  import '@fontsource/alegreya/400.css'
+  import '@fontsource/alegreya/500.css'
+  import '@fontsource/alegreya/700.css'
+  import '@fontsource/alegreya-sc/400.css'
+  import '@fontsource/alegreya-sc/500.css'
   import '$lib/styles/app.css'
   import '$lib/styles/app-preview.css'
 </script>
