8418a1581338bebfdaa58c430301193abe1654d0
Tim G. Thomas <tim@timgthomas.com>
2022-10-06 20:23:04 -0500

    switch plausible to use outbound link tracking, as well

--- src/routes/preview/+page.svelte
+++ src/routes/preview/+page.svelte
@@ -29,7 +29,7 @@
     <meta property="og:title" content="Faire Day: Faire and Festival Weather">
     <title>Faire Day: Faire and Festival Weather</title>
   {/if}
-  <script defer data-domain="faireday.com" src="https://plausible.io/js/plausible.js"></script>
+  <script defer data-domain="faireday.com" src="https://plausible.io/js/script.outbound-links.js"></script>
 </svelte:head>
 
 <Title title={data.title} isActive={isActive} />
