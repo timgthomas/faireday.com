68a82ccf69a6779edf16adcd03d245f9d1adc3f9
Tim G. Thomas <tim@timgthomas.com>
2022-10-16 14:51:19 -0500

    enable preview for trf pirate weekend

--- src/routes/preview/+page.server.js
+++ src/routes/preview/+page.server.js
@@ -1,9 +1,9 @@
 const baseUrl = 'https://faireday-api.vercel.app/api/v3'
 // const baseUrl = 'http://localhost:3000/api/v3'
 const faire = 'trf'
-const weekend = 'trf-2022-1'
-const title = 'Oktoberfest at TRF'
-const previewEnabled = false
+const weekend = 'trf-2022-3'
+const title = 'TRF’s Pirate Adventure'
+const previewEnabled = true
 
 export async function load() {
   if (!previewEnabled) return {}
