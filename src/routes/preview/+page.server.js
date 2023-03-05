38e7ca2b776c18185c8a5518550b0d7703a79f24
Tim G. Thomas <tim@timgthomas.com>
2023-03-04 21:30:21 -0600

    enable preview for sherwood week 2

--- src/routes/preview/+page.server.js
+++ src/routes/preview/+page.server.js
@@ -1,8 +1,8 @@
 const baseUrl = 'https://faireday-api.vercel.app/api/v3'
 // const baseUrl = 'http://localhost:3000/api/v3'
-const faire = 'larf'
-const weekend = 'larf-2022-6'
-const title = 'LARF’s Fireworks Finale'
+const faire = 'sherwood'
+const weekend = 'sherwood-2023-2'
+const title = 'Sherwood Forest Faire'
 const previewEnabled = true
 
 export async function load() {
