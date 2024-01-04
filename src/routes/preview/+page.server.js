84f32674449f6dff9bd08c1cba1ea5c3e6048403
Tim G. Thomas <github@timgthomas.com>
2024-01-04 17:27:47 -0600

    redirect preview users to the web app

--- src/routes/preview/+page.server.js
+++ src/routes/preview/+page.server.js
@@ -1,14 +1,5 @@
-const baseUrl = 'https://faireday-api.vercel.app/api/v3'
-// const baseUrl = 'http://localhost:3000/api/v3'
-const faire = 'sherwood'
-const weekend = 'sherwood-2023-8'
-const title = 'Sherwood’s Closing Weekend'
-const previewEnabled = false
+import { redirect } from '@sveltejs/kit'
 
 export async function load() {
-  if (!previewEnabled) return {}
-  const url = `${baseUrl}/faires/${faire}/weather?weekendId=${weekend}`
-  const weatherRequest = await fetch(url)
-  const weather = await weatherRequest.json()
-  return { ...weather, title }
+  throw redirect(307, 'https://app.faireday.com')
 }
